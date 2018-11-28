978243de81c1464e62321a80c01d88b76103bc02
kaeruko <mail@cloxs.jp>
2018-11-28 16:29:21 +0900

    css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
 
   def current_user
+  	helper_method :current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
